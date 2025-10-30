0a36de1e98aee232572338c4d2b2bbe46466eb5f
E. Triantafyllou <irenetriantafyllou@msn.com>
2025-10-30 19:59:12 +0200

    User endpoints

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,12 +2,22 @@ import { Request, Response, NextFunction } from "express";
 import * as userService from '../services/user.service';
 
 export const list = async(req: Request, res: Response, next: NextFunction) => {
-
   try {
     const result = await userService.findAllUsers();
     res.status(201).json(result);
   } catch(err) {
     next(err);
+  }
+}
+
+export const getOne = async(req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await userService.findUserById(req.params.id!)
+    if (!result) 
+      return res.status(404).json({message: "User not found"})
+    res.status(201).json(result);   
+  } catch(err) {
+    next(err)
   }
 }
 
@@ -19,3 +29,22 @@ export const create = async(req: Request, res: Response, next: NextFunction) => 
     next(err)
   }
 }
+
+export const update = async(req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await userService.updateUser(req.params.id!, req.body);
+    res.status(201).json(result);
+  } catch(err) {
+    next(err)
+  }
+}
+
+export const remove = async(req: Request, res: Response, next: NextFunction) => {
+  try {
+   const result = await userService.deleteUser(req.params.id!)
+   res.status(201).json(result);
+  } catch(err) {
+    next(err)
+  }
+}
+
