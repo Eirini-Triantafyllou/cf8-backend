d0e2a221463340e1b887207e09b7ea1b79475881
E. Triantafyllou <irenetriantafyllou@msn.com>
2025-10-30 19:07:45 +0200

    Create User

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,11 +2,20 @@ import { Request, Response, NextFunction } from "express";
 import * as userService from '../services/user.service';
 
 export const list = async(req: Request, res: Response, next: NextFunction) => {
-  
+
   try {
     const result = await userService.findAllUsers();
     res.status(201).json(result);
   } catch(err) {
     next(err);
+  }
+}
+
+export const create = async(req: Request, res: Response, next: NextFunction) => {
+  try {
+   const result = await userService.createUser(req.body);
+   res.status(201).json(result);
+  } catch(err) {
+    next(err)
   }
 }
