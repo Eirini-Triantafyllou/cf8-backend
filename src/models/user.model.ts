1390b2d206137b775a0cc9d339383a2360ec784a
E. Triantafyllou <irenetriantafyllou@msn.com>
2025-10-30 09:12:56 +0200

    initialize

---
+++ src/models/user.model.ts
@@ -0,0 +1,64 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface IPhone {type: string; number: string}
+
+export interface IUser extends Document {
+  username: string;
+  password: string;
+  firstname?: string;
+  lastname?: string;
+  email?: string;
+  address?: {
+    area?: string;
+    street?: string;
+    number?: string;
+    po?: string;
+    municipality?: string;
+  },
+  phone?: IPhone[]
+}
+
+const PhoneSchema = new Schema({
+  type: String, 
+  number: String
+},
+//  {_id:false} inserts _id or not
+)
+
+const AddressSchema = new Schema({
+    area: String,
+    street: String,
+    number: String,
+    po: String,
+    municipality: String
+})
+
+const UserSchema = new Schema({
+  username: { 
+    type: String, 
+    required: [true, "Username is required field"],
+    unique: true,
+    max: 20,
+    min: 4,
+    trim: true,
+    lowercase: true 
+  },
+  password: { type: String, required: true },
+  firstname: String,
+  lastname: { type: String },
+  email: { type: String, index: true},
+  // address: {
+  //   area: String,
+  //   street: String,
+  //   number: String,
+  //   po: String,
+  //   municipality: String
+  // },
+  address: [AddressSchema],
+  phone: { type:[PhoneSchema], null: true }
+},{
+  collection: "users",
+  timestamps: true,
+});
+
+export default model("User", UserSchema)
