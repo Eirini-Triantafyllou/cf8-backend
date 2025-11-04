a21ff4e52fc35bb345c941b720d10742251eb6aa
E. Triantafyllou <irenetriantafyllou@msn.com>
2025-11-04 18:55:40 +0200

    Swagger end points

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,10 +1,11 @@
+
 import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import mongooseToSwagger from "mongoose-to-swagger";
 import {Express} from 'express';
 import Role from './models/role.model';
 import User from './models/user.model';
- 
+
 const options: swaggerJSDoc.Options = {
   definition: {
     openapi: "3.0.0",
@@ -36,9 +37,9 @@ const options: swaggerJSDoc.Options = {
   },
   apis: ['./src/routes/*.ts']
 };
- 
+
 export const swaggerSpec = swaggerJSDoc(options);
- 
+
 export const setupSwagger = (app:Express) => {
   app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-}
+} 
